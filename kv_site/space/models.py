2384f2a9409e38e58c53e97d35b4de95106e6b28
qjlfkhlsdjfks <smnxnxnxmm@gmail.com>
2024-01-27 11:51:31 +0200

    re

--- kv_site/space/models.py
+++ kv_site/space/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Card(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField()
-    color = models.CharField(max_length=10, default='#a74d47')
     is_online = models.BooleanField(null=True)
     address = models.CharField(max_length=60, null=True)
     img = models.ImageField(upload_to='images')
